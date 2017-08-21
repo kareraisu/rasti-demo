ff320ce2b0ebe08b282a52d095de2e7f62b8bb47
Alejandro <impera87@gmail.com>
2017-08-21 19:30:31 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(__dirname))
+
+const REST_PORT = (process.env.PORT || 5000)
+
+app.listen(REST_PORT, () => {
+  console.log('App ready on port ' + REST_PORT)
+})
